025e9e1749fdd33e852c3becd921e2dc482ed88e
uio <96638103+uiojaafdosijfafdssf@users.noreply.github.com>
2022-07-17 19:39:10 -0700

    Update SaveInstanceKRNL.lua

--- loadstrings/SaveInstanceKRNL.lua
+++ loadstrings/SaveInstanceKRNL.lua
@@ -1,3 +1,4 @@
 local chatsaved = false
 local kickaftersave = true
+--sorry those things dont even work idk how to add variables to loadstrings lol
 loadstring(game:HttpGet("https://raw.githubusercontent.com/uiojaafdosijfafdssf/roblox/main/scripts/SaveInstanceKRNL.lua"))()
