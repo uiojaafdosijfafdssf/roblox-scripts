fa0b04b0357e80f009c3b6c5c477d4d87d59a5cd
uio <96638103+uiojaafdosijfafdssf@users.noreply.github.com>
2022-07-17 19:18:38 -0700

    Update SaveInstanceKRNL.lua

--- scripts/SaveInstanceKRNL.lua
+++ scripts/SaveInstanceKRNL.lua
@@ -1,6 +1,9 @@
 if kickaftersave ~= nil then else
     local kickaftersave = true
 end
+if chatsaved ~= nil then else
+    local chatsaved = false
+end
 
 makefolder("GamesLOL")
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
@@ -10,11 +13,14 @@ saveinstance(game, "GamesLOL/" .. GameName .. " " .. game.PlaceId, {
     DecompileTimeout = math.huge
 })
 warn('"' .. GameName .. '"  has been downloaded.')
-game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I have downloaded this game (" .. GameName .. ").","All")
-wait(0.5)
-game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I hope you don't mind.","All")
-wait(0.5)
-game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(":D","All")
+if chatsaved == true then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I have downloaded this game (" .. GameName .. ").","All")
+    wait(0.5)
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I hope you don't mind.","All")
+    wait(0.5)
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(":D","All")
+end
+
 if kickaftersave == true then
     wait(0.5)
     game.Players.LocalPlayer:Kick('Hello. You got kicked by the "save instance" script. There was not really a purpose to kick you, but I think that you should leave the game after you get kicked. You can disable this in the script.')
