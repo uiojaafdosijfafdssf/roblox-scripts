7678763bd107adc563dde71e5e8a450a4b314502
uio <96638103+uiojaafdosijfafdssf@users.noreply.github.com>
2022-07-18 13:57:35 -0700

    Update SaveInstanceKRNL.lua

--- scripts/SaveInstanceKRNL.lua
+++ scripts/SaveInstanceKRNL.lua
@@ -2,7 +2,7 @@ makefolder(foldername)
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 print('Downloading "' .. GameName .. '".')
 saveinstance(game, foldername .. "/" .. GameName .. " " .. game.PlaceId, {
-    IgnoreDefaultProps = IgnoreDefaultPropsOption
+	IgnoreDefaultProps = IgnoreDefaultPropsOption
 	Callback = CallbackOption
 	Binary = BinaryOption
 	Decompile = DecompileOption
@@ -18,14 +18,14 @@ saveinstance(game, foldername .. "/" .. GameName .. " " .. game.PlaceId, {
 })
 print('"' .. GameName .. '"  has been downloaded.')
 if chataboutsteal == true then
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I have downloaded this game (" .. GameName .. ").","All")
-    wait(timebetweenmessages)
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I hope you don't mind.","All")
-    wait(timebetweenmessages)
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(":D","All")
-    wait(timebetweenmessages)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I have downloaded this game (" .. GameName .. ").","All")
+	wait(timebetweenmessages)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I hope you don't mind.","All")
+	wait(timebetweenmessages)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(":D","All")
+	wait(timebetweenmessages)
 end
 
 if kickaftersave == true then
-    game.Players.LocalPlayer:Kick('Hello. You got kicked by the "save instance" script. There was not really a purpose to kick you, but I think that you should leave the game after you get kicked. You can disable this in the script.')
+	game.Players.LocalPlayer:Kick('Hello. You got kicked by the "save instance" script. There was not really a purpose to kick you, but I think that you should leave the game after you get kicked. You can disable this in the script.')
 end
