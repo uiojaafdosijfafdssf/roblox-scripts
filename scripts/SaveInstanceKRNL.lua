8f8328aa300eb38dc11f98bc32f48f7370a104a1
uio <96638103+uiojaafdosijfafdssf@users.noreply.github.com>
2022-07-18 13:20:18 -0700

    changed the "waits"
    i changed the time in between the chat messages from 0.5 to 0.01 (now that i think about it i should make that a setting), added a wait after the last chat message, and removed the wait in the kick thing

--- scripts/SaveInstanceKRNL.lua
+++ scripts/SaveInstanceKRNL.lua
@@ -8,13 +8,13 @@ saveinstance(game, foldername .. "/" .. GameName .. " " .. game.PlaceId, {
 print('"' .. GameName .. '"  has been downloaded.')
 if chataboutsteal == true then
     game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I have downloaded this game (" .. GameName .. ").","All")
-    wait(0.5)
+    wait(waitoption)
     game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I hope you don't mind.","All")
-    wait(0.5)
+    wait(waitoption)
     game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(":D","All")
+    wait(waitoption)
 end
 
 if kickaftersave == true then
-    wait(0.5)
     game.Players.LocalPlayer:Kick('Hello. You got kicked by the "save instance" script. There was not really a purpose to kick you, but I think that you should leave the game after you get kicked. You can disable this in the script.')
 end
