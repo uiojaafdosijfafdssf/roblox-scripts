1e86cc95273002ea16c4db9211a9f7055513ac05
uio <96638103+uiojaafdosijfafdssf@users.noreply.github.com>
2022-07-17 19:13:42 -0700

    Add files via upload

---
+++ scripts/savegame.lua
@@ -0,0 +1,21 @@
+if kickaftersave ~= nil then else
+    local kickaftersave = true
+end
+
+makefolder("GamesLOL")
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+warn('Downloading "' .. GameName .. '".')
+saveinstance(game, "GamesLOL/" .. GameName .. " " .. game.PlaceId, {
+    Decompile = true,
+    DecompileTimeout = math.huge
+})
+warn('"' .. GameName .. '"  has been downloaded.')
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I have downloaded this game (" .. GameName .. ").","All")
+wait(0.5)
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I hope you don't mind.","All")
+wait(0.5)
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(":D","All")
+if kickaftersave == true then
+    wait(0.5)
+    game.Players.LocalPlayer:Kick('Hello. You got kicked by the "save game" script. There was not really a purpose to kick you, but I think that you should leave the game after you get kicked. You can disable this in the script.')
+end
