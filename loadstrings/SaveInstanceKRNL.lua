cfd1672f038f88c52b363a9f5d0902da1c83ff5b
uio <96638103+uiojaafdosijfafdssf@users.noreply.github.com>
2022-07-18 12:11:41 -0700

    added folder name option

--- loadstrings/SaveInstanceKRNL.lua
+++ loadstrings/SaveInstanceKRNL.lua
@@ -2,9 +2,10 @@
 getgenv().chataboutsteal = false --basically it just makes you say "I have stolen this game, bye! :D" after u download a game
 getgenv().kickaftersave = true --this kicks you once u are done downloading the game
 getgenv().gamenameinfilename = false --this puts the name of the saved game in the file name (if it was on and u used this script on "epic gamer land", it would name the file "epic gamer land.rbxl".)
-getgenv().gameidinfilename = true -- this puts the id of the saved game in the file name (if it was on and u used this script on "Line Runner" by "Templates", it would name the file "301530843.rbxl".)
+getgenv().gameidinfilename = true --this puts the id of the saved game in the file name (if it was on and u used this script on "Line Runner" by "Templates", it would name the file "301530843.rbxl".)
 getgenv().customfilename = false --if you enable this, you get to name the file whatever you want. you can change the name in the setting below.
 getgenv().filename = "hi" --this sets the custom file name. (its a string, so if u put "CoolGame" there instead of "hi", it would name the file "CoolGame.rbxl".)
+getgenv().foldername = "GamesLOL" --this sets the name of the folder it should put the file into. (if u set it to "games i stole", it would put it into "workspace\games i stole")
 
 --LOADSTRING--
 loadstring(game:HttpGet("https://raw.githubusercontent.com/uiojaafdosijfafdssf/roblox/main/scripts/SaveInstanceKRNL.lua"))()
