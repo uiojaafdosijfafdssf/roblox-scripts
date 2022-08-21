6b67d63ddef89df70fa1b5fe0b2e0ef4ed6a7655
uio <96638103+uiojaafdosijfafdssf@users.noreply.github.com>
2022-08-21 15:35:18 -0700

    Update uioChatbot.lua

--- scripts/uioChatbot.lua
+++ scripts/uioChatbot.lua
@@ -42,7 +42,8 @@ local function messagepro(message, player) --In this function the magic happens
     local Response = game:HttpGet("https://api.simsimi.net/v2/?text=" ..
         text .. "&lc=" .. ChatbotLanguage .. "&cf=" .. ChatbotFilter .. "&api=" .. player.UserId) --The Simsimi api is called
     local data = HttpService:JSONDecode(Response)
-    local first200characters = string.gsub(data.success, "zex", "___")
+    local workingresponse = data.success
+    local first200characters = string.gsub(workingresponse, "zex", "___")
     local first200characters = string.gsub(first200characters, "love", "____")
     local first200characters = string.gsub(first200characters, "baby", "____")
     print("Chatbot got a response: " .. first200characters)
